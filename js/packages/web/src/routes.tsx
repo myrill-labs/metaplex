57e422eb7a626352d74a49672ccec34023326ada
bacarif <arif.bachir@hotmail.com>
2021-11-27 14:23:23 +0100

    Add route

--- js/packages/web/src/routes.tsx
+++ js/packages/web/src/routes.tsx
@@ -1,5 +1,5 @@
-import {HashRouter, Route, Switch} from 'react-router-dom';
-import {Providers} from './providers';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Providers } from './providers';
 import {
   AnalyticsView,
   ArtCreateView,
@@ -12,8 +12,8 @@ import {
   HomeView,
   StaticPageView,
 } from './views';
-import {AdminView} from './views/admin';
-import {BillingView} from './views/auction/billing';
+import { AdminView } from './views/admin';
+import { BillingView } from './views/auction/billing';
 
 export function Routes() {
   return (
@@ -21,13 +21,42 @@ export function Routes() {
       <HashRouter basename={'/'}>
         <Providers>
           <Switch>
+            <Route exact path="/admin" component={() => <AdminView />} />
+            <Route
+              exact
+              path="/analytics"
+              component={() => <AnalyticsView />}
+            />
+            <Route
+              exact
+              path="/art/create/:step_param?"
+              component={() => <ArtCreateView />}
+            />
             <Route
               exact
               path="/myrmidons/:id?"
-              component={() => <ArtworksView/>}
+              component={() => <ArtworksView />}
             />
-            <Route path="/about" component={() => <StaticPageView/>}/>
-            <Route path="/" component={() => <HomeView/>}/>
+            <Route exact path="/art/:id" component={() => <ArtView />} />
+            <Route exact path="/artists/:id" component={() => <ArtistView />} />
+            <Route exact path="/artists" component={() => <ArtistsView />} />
+            <Route
+              exact
+              path="/auction/create/:step_param?"
+              component={() => <AuctionCreateView />}
+            />
+            <Route
+              exact
+              path="/auction/:id"
+              component={() => <AuctionView />}
+            />
+            <Route
+              exact
+              path="/auction/:id/billing"
+              component={() => <BillingView />}
+            />
+            <Route path="/about" component={() => <StaticPageView />} />
+            <Route path="/" component={() => <HomeView />} />
           </Switch>
         </Providers>
       </HashRouter>
